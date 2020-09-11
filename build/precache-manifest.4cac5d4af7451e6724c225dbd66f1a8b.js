7a176bbee97a46263144d84ca805e06a95dc2840
Jordan Cheng <51500782+jordancheng604@users.noreply.github.com>
2020-09-11 11:52:06 -0700

    Build folder for prod.

---
+++ build/precache-manifest.4cac5d4af7451e6724c225dbd66f1a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ce1f50b0ad613c23095f35ded9d45e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757c55b8fd45a95b693b",
+    "url": "/static/css/main.51eec01b.chunk.css"
+  },
+  {
+    "revision": "07cd0eb641f32d0052f0",
+    "url": "/static/js/2.008e52c0.chunk.js"
+  },
+  {
+    "revision": "1fe0eac8c1c29ba3b67059278117f076",
+    "url": "/static/js/2.008e52c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757c55b8fd45a95b693b",
+    "url": "/static/js/main.b3e80d9f.chunk.js"
+  },
+  {
+    "revision": "cf4439210bad62703faa",
+    "url": "/static/js/runtime-main.c60e430d.js"
+  },
+  {
+    "revision": "694dd3775c0d7d7da813f9627eafea98",
+    "url": "/static/media/logoChinook2.694dd377.svg"
+  }
+]);
